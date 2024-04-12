6645a2d5cdc18a433bfecc4bdb2a445677bccb01
Harshida Shaily <20BCS2198@gmail.com>
2024-04-12 15:30:28 +0530

    Seieve Of Erathnosis

--- src/main/scala/PrimeNumber.scala
+++ src/main/scala/PrimeNumber.scala
@@ -1,15 +1,15 @@
 object PrimeNumber{
   def SeieveOfErathnoisis(n: Int):Int={
     if (n <= 0) throw new IllegalArgumentException("n must be a positive integer")
-    val limit = n * (math.log(n) + math.log(math.log(n))).toInt
-    val sieve = Array.fill(limit + 1)(true)
-    sieve(0) = false
-    sieve(1) = false
-    var count = 0
-    for (p <- 2 to limit if sieve(p)) {
-      count += 1
-      if (count == n) return p
-      for (i <-p* p to limit by p) {
+    val limit=n *(math.log(n) + math.log(math.log(n))).toInt
+    val sieve =Array.fill(limit + 1)(true)
+    sieve(0)=false
+    sieve(1)=false
+    var cnt=0
+    for (p<-2 to limit if sieve(p)) {
+      cnt+=1
+      if (cnt==n) return p
+      for (i <-p*p to limit by p) {
         sieve(i) = false
       }
     }
